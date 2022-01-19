dc6081039d23e516f39953d891b6fb49ee1331bc
Dominik Buczkowski <41574412+buczkowskid@users.noreply.github.com>
2022-01-19 12:12:58 +0100

    Delete aplikacja directory

--- aplikacja/connect.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-$host = "localhost";
-$db_user = "sbdgcrmg_test";
-$db_password = "test123";
-$db_name = "sbdgcrmg_test";
--- aplikacja/popup-reg.js
+++
@@ -1,6 +0,0 @@
-$(".home-header .regulamin").on("click", function () {
-  $(".reg-wrap").addClass("active");
-});
-$("span.exit").on("click", function () {
-  $(".reg-wrap").removeClass("active");
-});
